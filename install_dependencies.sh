644a5772ec7d0008d08dd19d26d6199083861218
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2024-06-25 22:56:33 -0300

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,8 +1,22 @@
-
 #!/bin/bash
 
-# Update the package list
+# Atualizar a lista de pacotes
 sudo apt-get update
 
-# Install system dependencies
-sudo apt-get install -y libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libsmpeg-dev libportmidi-dev libfreetype6-dev libportaudio2 portaudio19-dev
+# Instalar dependências do sistema
+sudo apt-get install -y \
+    libsdl2-dev \
+    libsdl2-image-dev \
+    libsdl2-mixer-dev \
+    libsdl2-ttf-dev \
+    libsmpeg-dev \
+    libportmidi-dev \
+    libfreetype6-dev \
+    libportaudio2 \
+    portaudio19-dev \
+    python3-dev \
+    build-essential \
+    pkg-config
+
+# Instalar dependências do Python
+pip install -r requirements.txt
