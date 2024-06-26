daa675bcc83537ba3b371ff230b703a59c86cf92
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2024-06-26 00:55:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import os
 load_dotenv() 
 
 # Inicializando a API Key do modelo
-groq_api_key = os.environ['GROQ_API_KEY']
+groq_api_key = os.environ['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']
 
 # Substituir o inicializador de ChatGroq por uma função de log ou um LLM disponível.
 # Por exemplo, substitua por um LLM local ou qualquer outro modelo compatível com langchain.
